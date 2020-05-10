8067d5bfaeea450eb74f0472628b3b3b575bfbb7
Kav Khalsa <kenny.khalsa@gmail.com>
2020-05-10 18:02:20 +0100

    5 jasmine tests passing

--- src/app/services/faqs.service.ts
+++ src/app/services/faqs.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { IFAQ } from '../interfaces/faq';
-import FAQS  from './faqs.json';
+import FAQS from './faqs.json';
 
 @Injectable({
   providedIn: 'root'
